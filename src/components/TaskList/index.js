ab35722797fca7d4059f0f128eafab2ff242cc07
Mahnoor Jamal <mahnoorjamal219@gmail.com>
2022-09-25 13:26:02 +0500

    Navigation

---
+++ src/components/TaskList/index.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+import { View, FlatList, Text } from "react-native";
+import CheckBox from '@react-native-community/checkbox';
+import { styles } from "./styles";
+import { Data } from "./Data";
+import Swipeable from 'react-native-gesture-handler/Swipeable';
+
+const RenderRight = () => {
+
+    return (
+        <View style={{ backgroundColor: "red" }}>
+            <Text>Delete</Text>
+        </View>
+    )
+}
+
+const RenderItem = ({ item, index }) => {
+
+    return (
+        <Swipeable renderRightActions={RenderRight}>
+            <View style={styles.item}>
+                <View>
+                    <CheckBox style={{ borderColor: '#DADADA', borderRadius: "10%" }} />
+                </View>
+
+
+                <View style={{ marginLeft: 7, marginTop: 4 }}>
+                    <Text style={styles.task}>{item.task}</Text>
+                    <Text style={styles.date}>{item.date}</Text>
+                </View>
+            </View>
+        </Swipeable>
+
+    )
+}
+
+export const TaskList = () => {
+    return (
+
+        <FlatList data={Data}
+            style={{ marginTop: 8 }}
+            renderItem={({ item, index }) => <RenderItem item={item} index={index} />} />
+
+    )
+}
