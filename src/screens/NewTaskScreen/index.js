a720f4660f4bb0da0c8cc1a47701f124cabd08da
Mahnoor Jamal <mahnoorjamal219@gmail.com>
2022-09-27 00:56:09 +0500

    PopUps

--- src/screens/NewTaskScreen/index.js
+++ src/screens/NewTaskScreen/index.js
@@ -6,17 +6,53 @@ import { styles } from "./styles";
 import { CustomText } from "../../components/CustomText";
 import Input from "../../components/Input";
 import Button from "../../components/Button";
+import Modal from "react-native-modal";
 
 export const NewTaskScreen = ({ navigation }) => {
+    const [isModalVisible, setIsModalVisible] = React.useState(true);
 
+    const handleModal = () => setIsModalVisible(() => !isModalVisible);
     return (
         <ScrollView style={styles.container}>
 
-                <Header backButton="true" title='New Task' onPress={() => navigation.navigate('TaskScreen')} />
-            
+            <Header backButton="true" title='New Task' onPress={() => navigation.navigate('TaskScreen')} />
+
+            <Modal isVisible={isModalVisible}>
+                <View style={{ flex: 1 }}>
+                    {/* <Text>Hello!</Text> */}
+
+                    <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+                <Text>
+                Reminder
+                </Text>
+              <Text style={styles.modalText}>
+             
+Consequat velit qui adipisicing sunt do reprehenderit ad laborum tempor ullamco exercitation.
+ Ullamco tempor adipisicing et voluptate duis sit esse aliqua esse ex dolore esse.
+ Consequat velit qui adipisicing sunt.
+              </Text>
+
+              <Button 
+              style={{
+              alignItems: "center",
+              backgroundColor: "#000000",
+              borderRadius: 100,
+              height: 51,
+              width: 200,
+              justifyContent: 'center',
+              left: 2,
+              right: 16}}
+              onPress={handleModal}
+               text="Remind me again"></Button>
+            </View> 
+          </View>
+
+                    {/* <Button title="Hide modal" onPress={handleModal} /> */}
+                </View>
+            </Modal>
 
 
-  
             {/* 
         <View style={styles.inputContainer}>
             <Input placeholder='Summary' style={styles.inputStyle} />
